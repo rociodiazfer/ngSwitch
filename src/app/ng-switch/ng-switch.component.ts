096655dffea7bc5ffc09a7a32b241ca28dc1912d
rocio <rocio.dafer.1@educa.jcyl.es>
2023-11-23 18:45:27 +0100

    FIRSTcommit

--- src/app/ng-switch/ng-switch.component.ts
+++ src/app/ng-switch/ng-switch.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 
 @Component({
   selector: 'app-ng-switch',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './ng-switch.component.html',
   styleUrl: './ng-switch.component.css'
 })
