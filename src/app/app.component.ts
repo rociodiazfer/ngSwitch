84525c1bf7b21738b1239adf96605c64d6279e16
rocio <rocio.dafer.1@educa.jcyl.es>
2023-11-23 18:44:53 +0100

    FIRSTcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {NgSwitchComponent} from "./ng-switch/ng-switch.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgSwitchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/ng-switch/ng-switch.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-ng-switch',
+  standalone: true,
+  templateUrl: './ng-switch.component.html',
+  styleUrl: './ng-switch.component.css'
+})
+export class NgSwitchComponent {
+constructor() {
+}
+ngOnInit(){
+
+}
+user=this.randomUser()
+numbers =[1,2,3,4,5,6,7,8,9];
+days = Days;
+day=this.randomDays()
+
+randomUser():string {
+  const id = Math.floor(Math.random()*3);
+  return Users[id]
+}
+randomDays():string{
+  const id = Math.floor(Math.random()*7)
+  return Days[id]
+}
+}
+export enum Users{
+  Ramesh,
+  Tom,
+  Tony
+}
+export enum Days{
+  SUNDAY,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+}
